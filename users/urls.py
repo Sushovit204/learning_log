c29321656a7dde736846c848b10a017630cbc77e
Sushovit204 <shahsushovit434@gmail.com>
2022-09-16 22:40:56 +0545

    Added the user login and authorization

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 """Defines  a url pattern for users"""
 from django.urls import path, include
 
-app_name = 'urls'
+app_name = 'users'
 urlpatterns = [
     # Include Default auth urls.
     path('', include('django.contrib.auth.urls'))
